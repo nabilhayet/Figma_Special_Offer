3c44647fd305694900f77458cd87b94b18652d6a
nabilhayet <nabil4457@yahoo.com>
2023-05-22 18:44:23 -0400

    added javascript

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+// let main = document.querySelector(".main")
 let body = document.querySelector(".body")
-let submitForm = document.querySelector(".submit")
+let form = document.querySelector('#post');
+let main = document.body
 
 let name = document.querySelector("#name")
 let email = document.querySelector("#email")
 let handle = document.querySelector("#handle")
 
-submitForm.addEventListener('click', function () {
+let submit = document.querySelector(".submit")
+let divTag = document.createElement("div")
+let pTag = document.createElement("p")
 
+submit.addEventListener("click", function () {
+
+    pTag.textContent = name.value
+    divTag.appendChild(pTag)
+
+    pTag.textContent = email.value
+    divTag.appendChild(pTag)
+
+    pTag.textContent = handle.value
+    divTag.appendChild(pTag)
+    console.log("We are here")
+
+    document.body.innerHTML = "";
+    document.body.style.backgroundImage = "none"
+    document.body.style.backgroundColor = "Yellow"
 
 });
