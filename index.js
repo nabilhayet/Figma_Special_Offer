c29b70afc5182624b8c8892dd0923b20c24aae36
nabilhayet <nabil4457@yahoo.com>
2023-05-22 15:23:14 -0400

    added html form and css

---
+++ index.js
@@ -0,0 +1,11 @@
+let body = document.querySelector(".body")
+let submitForm = document.querySelector(".submit")
+
+let name = document.querySelector("#name")
+let email = document.querySelector("#email")
+let handle = document.querySelector("#handle")
+
+submitForm.addEventListener('click', function () {
+
+
+});
